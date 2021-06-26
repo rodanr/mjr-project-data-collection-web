f34e1593700a5a9577203d4b743a72d306ddcd48
Raj Maharjan <rmaharjan.rm90@gmail.com>
2021-06-26 20:20:44 +0545

    version 1.0.0 done

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
 //imports
-const express = require("express");
-const dotenv = require("dotenv");
-const assert = require("assert");
-const { MongoClient } = require("mongodb");
+const express = require('express');
+const dotenv = require('dotenv');
+const assert = require('assert');
+const { MongoClient } = require('mongodb');
 //for unique name generation
-const { v4: uuidv4 } = require("uuid");
-const fs = require("fs-extra");
-const WebSocket = require("ws");
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs-extra');
+const WebSocket = require('ws');
 
 // for google drive uploads
 const { google } = require('googleapis');
 const path = require('path');
 const fsa = require('fs');
 
-const CLIENT_ID = '487485404362-8f3297hrttb74grvvb85a4qb2679hq4f.apps.googleusercontent.com';
+const CLIENT_ID =
+  '487485404362-8f3297hrttb74grvvb85a4qb2679hq4f.apps.googleusercontent.com';
 const CLIENT_SECRET = 'SnBz7MdBLC7bzuqsiMo2VI6g';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground/';
 
-const REFRESH_TOKEN = '1//049qC-mxHpZeECgYIARAAGAQSNwF-L9IruZRUgRqcmyzaKOiTQuvY4k7sWpRbeICDhKPVDnc3sQqQN9ecmOrVyA6q_3yxu0ISukI';
-
-
+const REFRESH_TOKEN =
+  '1//049qC-mxHpZeECgYIARAAGAQSNwF-L9IruZRUgRqcmyzaKOiTQuvY4k7sWpRbeICDhKPVDnc3sQqQN9ecmOrVyA6q_3yxu0ISukI';
 
 //initialization
 dotenv.config();
 const app = express();
-const thanksRouter = require("./routes/thanks");
-const welcomeRouter = require("./routes/welcome");
-const { log } = require("console");
-const hostname = "127.1.0.0";
+const thanksRouter = require('./routes/thanks');
+const welcomeRouter = require('./routes/welcome');
+const { log } = require('console');
+const hostname = '127.1.0.0';
 var sentenceId;
 var jsonData, gender;
 
-
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/static"));
-app.use("/thanks", thanksRouter);
-app.use("/", welcomeRouter);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+app.use('/thanks', thanksRouter);
+app.use('/', welcomeRouter);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -44,43 +43,38 @@ let fileName;
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
-  REDIRECT_URI  
+  REDIRECT_URI
 );
 
-oauth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-const drive = google.drive(
-  {
-      version: 'v3',
-      auth: oauth2Client
+const drive = google.drive({
+  version: 'v3',
+  auth: oauth2Client,
+});
+
+async function uploadFile(name) {
+  const dirPath = path.join(__dirname, 'uploads');
+  try {
+    const filePath = path.join(dirPath, name);
+    const response = await drive.files.create({
+      requestBody: {
+        name: name,
+        mimeType: 'audio/wav',
+        parents: ['1SfEFYnSoarrw7zjgSFf9XSNrYrj17KSl'],
+      },
+      media: {
+        mimeType: 'audio/wav',
+        body: fsa.createReadStream(filePath),
+      },
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
   }
-)
 
-
-
-async function uploadFile(name){
-  const dirPath = path.join(__dirname,'uploads')
-  try{
-      const filePath = path.join(dirPath, name);
-      const response = await drive.files.create({
-          requestBody:{
-              name: name,
-              mimeType: 'audio/wav',
-              parents:['1SfEFYnSoarrw7zjgSFf9XSNrYrj17KSl']
-          },
-          media:{
-              mimeType:'audio/wav',
-              body: fsa.createReadStream(filePath)
-          }
-      });
-      console.log(response.data);
-  }
-  catch(error){
-      console.log(error.message);
-  }
-  
-  fs.emptyDir(dirPath, ()=>{
-    console.log("Dir is now empty");
+  fs.emptyDir(dirPath, () => {
+    console.log('Dir is now empty');
   });
 }
 
@@ -90,7 +84,7 @@ function randomIntFromInterval(min, max) {
 }
 async function fetchJSON() {
   //reading data from data.json
-  data = fs.readFileSync("data.json", "utf-8");
+  data = fs.readFileSync('data.json', 'utf-8');
   return JSON.parse(data);
 }
 //data is passed to jsonData variable after promise is resolved from fetchJSON
@@ -99,13 +93,17 @@ fetchJSON().then((data) => {
 });
 function getSentence() {
   sentenceId = randomIntFromInterval(0, 10109);
-  return jsonData[sentenceId]["sentences"];
+  return jsonData[sentenceId]['sentences'];
 }
 const uploadDocs = function (db, callback) {
   // Get the documents collection
-  const collection = db.collection("data_collection");
+  const collection = db.collection('data_collection');
   // Insert some documents
-  var object = { sentenceId: sentenceId, fileName: fileName, gender: gender };
+  var object = {
+    sentenceId: sentenceId,
+    fileName: fileName,
+    gender: gender,
+  };
   collection.insertOne(object, function (err, result) {
     if (err) throw err;
   });
@@ -129,33 +127,30 @@ function generateFileName() {
   fileName = uuidv4();
 }
 
-app.post("/collect", async function (req, res) {
+app.post('/collect', function (req, res) {
   generateFileName();
   gender = req.body.gender;
-  res.render("index", {
+  res.render('index', {
     filename: fileName,
     sentence: getSentence(),
   });
 });
 
-
-
 //Receiving the audio data
 const wss = new WebSocket.Server({ port: process.env.port });
-wss.on("connection", (ws, req) => {
-  ws.on("message", (message) => {
+wss.on('connection', (ws, req) => {
+  ws.on('message', (message) => {
     fs.writeFile(
       `uploads/${fileName}.wav`,
       Buffer.from(new Uint8Array(message), function (err) {
         if (err) throw err;
-        console.log("Upload success");
+        console.log('Upload success');
       })
     ).then(() => {
-      
       uploadFile(`${fileName}.wav`);
       uploadToCollection();
       //triggers onmessage in client side sending event.data as "uploaded" string
-      ws.send("uploaded");
+      ws.send('uploaded');
     });
   });
 });
--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,29 @@
 //initializing
-const recordButton = $("#recordButton");
-const stopButton = $("#stopButton");
-const submitButton = $("#submitButton");
-const nextButton = $("#nextButton");
-const finishButton = $("#finishButton");
-const fileNameHolder = $("#fileNameHolder");
-const ws = new WebSocket("ws://localhost:3000/");
-let audioPlayer = $("#audioPlayer");
+const recordButton = $('#recordButton');
+const stopButton = $('#stopButton');
+const submitButton = $('#submitButton');
+const nextButton = $('#nextButton');
+const finishButton = $('#finishButton');
+const fileNameHolder = $('#fileNameHolder');
+const ws = new WebSocket('ws://localhost:3000/');
+let audioPlayer = $('#audioPlayer');
 let audioBlobGlobal;
 let recorder, audioURL;
+var finish = false;
 function toggleButtons(record, stop, submit, next, finish) {
   // send true to the respecting parameter name to turn on the button
   //toggling all buttons based on the variable values
-  recordButton.prop("disabled", !record);
-  stopButton.prop("disabled", !stop);
-  submitButton.prop("disabled", !submit);
-  nextButton.prop("disabled", !next);
-  finishButton.prop("disabled", !finish);
+  recordButton.prop('disabled', !record);
+  stopButton.prop('disabled', !stop);
+  submitButton.prop('disabled', !submit);
+  nextButton.prop('disabled', !next);
+  finishButton.prop('disabled', !finish);
 }
 function toggleFileNameHolder(show) {
   if (show === true) {
-    fileNameHolder.css("display", "block");
+    fileNameHolder.css('display', 'block');
   } else {
-    fileNameHolder.css("display", "none");
+    fileNameHolder.css('display', 'none');
   }
 }
 function startRecording() {
@@ -35,19 +36,19 @@ function startRecording() {
 }
 function stopRecording() {
   //toggling buttons
-  toggleButtons(true, false, true, false, false);
+  toggleButtons(true, false, true, false, true);
   toggleFileNameHolder(true); //showing filename
   recorder.stop();
 }
 
 function playAudio(audioBlob) {
   audioBlobGlobal = audioBlob;
-  audioPlayer.attr("src", URL.createObjectURL(audioBlobGlobal.data));
+  audioPlayer.attr('src', URL.createObjectURL(audioBlobGlobal.data));
   audioURL = window.URL.createObjectURL(audioBlobGlobal.data);
   fileName = fileNameHolder.text();
-  $("#downloadButton").attr("href", audioURL);
-  $("#downloadButton").attr("download", `${fileName}`);
-  audioPlayer[0].play();
+  $('#downloadButton').attr('href', audioURL);
+  $('#downloadButton').attr('download', `${fileName}`);
+  // audioPlayer[0].play(); //Auto play after recording
 }
 
 function submitAudio() {
@@ -72,12 +73,19 @@ if (navigator.mediaDevices) {
   recordButton.click(startRecording);
   stopButton.click(stopRecording);
   submitButton.click(submitAudio);
-  finishButton.click(() => (window.location.href = "/thanks"));
+  finishButton.click(() => {
+    finish = true;
+    submitAudio();
+  });
   nextButton.click(() => location.reload()); //refreshing the page to get new text to read and get new filename
   //monitoring if the file is uploaded
   ws.onmessage = function (event) {
-    if (event.data === "uploaded") {
-      alert("Successfully uploaded");
+    if (event.data === 'uploaded') {
+      if (finish) {
+        window.location.href = '/thanks';
+      } else {
+        location.reload();
+      }
     }
   };
 }

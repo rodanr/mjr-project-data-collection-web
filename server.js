41a343bed5833e1702a5d0df2619a61bc70421b9
Raj Maharjan <rmaharjan.rm90@gmail.com>
2021-06-29 10:00:06 +0545

    mac address taking kept and storage changed

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { MongoClient } = require('mongodb');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs-extra');
 const WebSocket = require('ws');
+const os = require('os');
 
 // for google drive uploads
 const { google } = require('googleapis');
@@ -14,12 +15,12 @@ const path = require('path');
 const fsa = require('fs');
 
 const CLIENT_ID =
-  '487485404362-8f3297hrttb74grvvb85a4qb2679hq4f.apps.googleusercontent.com';
-const CLIENT_SECRET = 'SnBz7MdBLC7bzuqsiMo2VI6g';
+  '425991307248-p93kkhatt8qgk7qqj4chh83prolcn7af.apps.googleusercontent.com';
+const CLIENT_SECRET = 'gs71h1y506xfuItD4YoRsUWg';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground/';
 
 const REFRESH_TOKEN =
-  '1//049qC-mxHpZeECgYIARAAGAQSNwF-L9IruZRUgRqcmyzaKOiTQuvY4k7sWpRbeICDhKPVDnc3sQqQN9ecmOrVyA6q_3yxu0ISukI';
+  '1//04yv1C5kNFVG6CgYIARAAGAQSNwF-L9IrscrFqPPN5BnEd4ZeaSmPJYqIJLirA8hJ9hPOy7NIB_lKbElh0TZtsR2CSpiu6Mt4M2c';
 
 //initialization
 dotenv.config();
@@ -29,9 +30,21 @@ const welcomeRouter = require('./routes/welcome');
 const { log } = require('console');
 const hostname = '127.1.0.0';
 var sentenceId;
-var jsonData, gender;
+var jsonData, gender, mac;
 
 app.set('view engine', 'ejs');
+app.use((req, res, next) => {
+  // Giving user an Id
+  let values = os.networkInterfaces();
+  if ('Ethernet' in values) {
+    mac = values['Ethernet'][0]['mac'];
+  } else if ('WiFi' in values) {
+    mac = values['WiFi'][0]['mac'];
+  } else {
+    mac = Date.now() + Math.random();
+  }
+  next();
+});
 app.use(express.static(__dirname + '/static'));
 app.use('/thanks', thanksRouter);
 app.use('/', welcomeRouter);
@@ -61,7 +74,7 @@ async function uploadFile(name) {
       requestBody: {
         name: name,
         mimeType: 'audio/wav',
-        parents: ['1SfEFYnSoarrw7zjgSFf9XSNrYrj17KSl'],
+        parents: ['1u6_4EijMdM8OZBFrcjNSQE9SqNgDP-e3'],
       },
       media: {
         mimeType: 'audio/wav',
@@ -100,6 +113,7 @@ const uploadDocs = function (db, callback) {
   const collection = db.collection('data_collection');
   // Insert some documents
   var object = {
+    userId: mac,
     sentenceId: sentenceId,
     fileName: fileName,
     gender: gender,

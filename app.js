b85de35a0a8d78ba134a80479e60699dec77fdbf
Rodan <rodanramdam0818@gmail.com>
2021-07-01 23:08:06 +0545

    Changed the port

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(8000, (err, done) => {
+app.listen(process.env.PORT || 8000, (err, done) => {
   if (err) {
     console.log('Error');
   } else {
